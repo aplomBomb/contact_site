f2226a0a1dc9b56228a20822e935ba4462d992d4
aplombomb <seizethesteez@gmail.com>
2020-10-17 12:38:27 -0400

    🍊 initial commit|router setup 🍊

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, About, Four0Four } from "./components";
+import "./App.css";
+
+function App() {
+  let routes = (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route>
+        <Four0Four />
+      </Route>
+    </Switch>
+  );
+  return <div className="App"><Router>{routes}</Router></div>;
+}
+
+export default App;
---
+++ src/components/Four0Four/Four0Four.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Four0Four = () => {
+  return (
+    <div>
+      <h1>404</h1>
+    </div>
+  );
+};
